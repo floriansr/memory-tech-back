6c88672dc8b26624eb9d5c17516f8cbf43377562
floriansr <sueurfloriansf@gmail.com>
2020-07-30 10:51:59 +0200

    [add] seed csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+CSV.foreach(Rails.root.join('lib/memory-tech-challenge-data.csv'), headers: true) do |row|
+  
+  Transaction.create({
+    date: row[0],
+    order_id: row[1],
+    customer_id: row[2],
+    country: row[3],
+    product_code: row[4],
+    product_description: row[5],
+    quantity: row[6],
+    unit_price: row[7],
+  })
+end
